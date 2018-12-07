a474f530a021ff01675fa829ec648623ec8e7477
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-07 20:14:23 +0300

    Working with properties

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,26 @@ let h1 = React.createElement('h1', null, 'Hello World');
 let h2 = React.createElement('h2', null, 'Welcome to planet earth');
 class HelloWorld extends React.Component {
     render() {
-        return React.createElement('div', null, h1, h2)
+        return React.createElement('h1', this.props, 'Hello ' + this.props.frameworkName + ' World')
     }
 }
 
 ReactDOM.render(
    React.createElement(
        'div', null,
-       React.createElement(HelloWorld),
-       React.createElement(HelloWorld),
-       React.createElement(HelloWorld)), document.getElementById('content')
+       React.createElement(HelloWorld, {
+           id: 'ember',
+           frameworkName: 'Ember.js',
+           title: 'A framework for creating ambitious web applications'
+       }),
+       React.createElement(HelloWorld, {
+           id: 'backbone',
+           frameworkName: 'Backbone.js',
+           title: 'Backbone.js gives structure to web applications...'
+       }),
+       React.createElement(HelloWorld, {
+           id: 'angular',
+           frameworkName: 'Angular.js',
+           title: 'Superheroic JavaScript MVW Framework no puns intended'
+       })), document.getElementById('content')
 );
