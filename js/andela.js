501d1eaed055c94370110142fef21fc6f7d724f4
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-08 10:20:47 +0300

    Props with Class component

--- js/andela.js
+++ js/andela.js
@@ -1,15 +1,15 @@
 // component is a js class/function that optionally
 // accepts inputs i.e props and returns a react element that
 // describes how a section of the ui should appear
-const Greeting = (props) => <h1>Hello {props.name}!</h1>; //functional component
-ReactDOM.render(
-    <Greeting />,
-    document.getElementById('content')
-);
-// class Greeting extends React.Component {
-//     render() {
-//         return <h1>Hello World!</h1>;
-//     }
-// }
+// const Greeting = (props) => <h1>Hello {props.name}!</h1>; //functional component
+// ReactDOM.render(
+//     <Greeting />,
+//     document.getElementById('content')
+// );
+class Greeting extends React.Component {
+    render() {
+        return <h1>Hello {this.props.name}!</h1>;
+    }
+}
 
 ReactDOM.render(<Greeting name={`Daniel`}/>, document.getElementById('content'));
