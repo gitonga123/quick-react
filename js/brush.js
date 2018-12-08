cff4bf47c60d58a98aa09fc7278404878c0d2326
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-08 09:39:40 +0300

    Spread Operator

---
+++ js/brush.js
@@ -0,0 +1,7 @@
+// something new today smilely face
+const east = ['Kenya', 'Uganda', 'Tanzania'];
+const west = ['Nigeria', 'Cameroon', 'Ghana', 'Niger'];
+
+const countries = [...east, ...west];
+
+console.log(countries.length);
