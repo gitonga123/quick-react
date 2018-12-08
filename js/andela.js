3d5c9c0f8238d8820e2a862e74e72db1170604c1
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-08 10:03:40 +0300

    Class (Stateful) components

--- js/andela.js
+++ js/andela.js
@@ -1,8 +1,15 @@
 // component is a js class/function that optionally
 // accepts inputs i.e props and returns a react element that
 // describes how a section of the ui should appear
-const Greeting = () => <h1>Hello World today!</h1>; //functional component
-ReactDOM.render(
-    <Greeting />,
-    document.getElementById('content')
-);
+// const Greeting = () => <h1>Hello World today!</h1>; //functional component
+// ReactDOM.render(
+//     <Greeting />,
+//     document.getElementById('content')
+// );
+class Greeting extends React.Component {
+    render() {
+        return <h1>Hello World!</h1>;
+    }
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('content'));
