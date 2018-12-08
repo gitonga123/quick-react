00d5db8924cd93071246e1f8c95d18929e54e871
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-08 11:43:05 +0300

    Lexical Scoping

--- js/brush.js
+++ js/brush.js
@@ -3,5 +3,18 @@ const east = ['Kenya', 'Uganda', 'Tanzania'];
 const west = ['Nigeria', 'Cameroon', 'Ghana', 'Niger'];
 
 const countries = [...east, ...west];
+// global scope
+const firstName = 'Ann';
+myFunction = () => {
+    // local scope
+    const secondName = 'Mutwiri';
+    console.log(firstName, secondName);
+    nestedFunction = () => {
+        const thirdName = 'Kalekye';
+        console.log(firstName, secondName, thirdName);
+    };
 
-console.log(countries.length);
+    nestedFunction();
+};
+
+myFunction();
