1713592343b943976e1437440cc5ae374b23ecbd
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-12 08:38:59 +0300

    Working with JSX in components

--- js/script.jsx
+++ js/script.jsx
@@ -3,7 +3,19 @@
 //     document.getElementById('content')
 // );
 
-let HelloWorld = <h1>Hello World</h1>;
+let helloWorld = <h1>Hello World</h1>;
 ReactDOM.render(
-    HelloWorld, document.getElementById('content')
+    helloWorld, document.getElementById('content')
 );
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello KENYA</h1>
+                <h2>How are you doing?</h2>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<HelloWorld />, document.getElementById('content'))
