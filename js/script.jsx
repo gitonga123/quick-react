4501fb493c8a4152ae85c522c6dfec6f36b91602
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-12 08:48:02 +0300

    Outputting variables in JSX

--- js/script.jsx
+++ js/script.jsx
@@ -3,17 +3,19 @@
 //     document.getElementById('content')
 // );
 
-let helloWorld = <h1>Hello World</h1>;
-ReactDOM.render(
-    helloWorld, document.getElementById('content')
-);
+// let helloWorld = <h1>Hello World</h1>;
+// ReactDOM.render(
+//     helloWorld, document.getElementById('content')
+// );
 
 class HelloWorld extends React.Component {
     render() {
+        let dateTimeNow = new Date().toLocaleString();
         return (
             <div>
                 <h1>Hello KENYA</h1>
                 <h2>How are you doing?</h2>
+                <h3>Current Date Time = {dateTimeNow}</h3>
             </div>
         )
     }
