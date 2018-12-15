44fb4018dcf8924f41db2e87f95c7a6772e05477
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-15 14:27:21 +0300

    Replace React createElement Function Call with JSX

--- egghead/js/index.js
+++ egghead/js/index.js
@@ -1,3 +1,10 @@
 const rootElement = document.getElementById('root');
-const element = React.createElement('div', {className: "container", children: "Hello World"});
+// const element = React.createElement('div', {className: "container", children: "Hello World"});
+const greeting = "Hello There";
+const myClassName = "container";
+const props = {
+    className: 'container',
+    children: 'Hello World'
+}
+const element = <div {...props}/>
 ReactDOM.render(element,rootElement);
