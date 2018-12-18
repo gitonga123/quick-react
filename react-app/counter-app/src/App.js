61bcaa3ee336414810b12321195f60d6ddd91754
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-18 12:34:42 +0300

    stateless functional component

--- react-app/counter-app/src/App.js
+++ react-app/counter-app/src/App.js
@@ -4,12 +4,50 @@ import NavBar from "./components/navbar.jsx";
 import Counters from "./components/counters.jsx";
 
 class App extends Component {
+  state = {
+    counters: [
+      { key: 0, value: 0 },
+      { key: 1, value: 1 },
+      { key: 2, value: 2 },
+      { key: 3, value: 3 },
+      { key: 4, value: 4 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.key !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+          />
         </main>
       </React.Fragment>
     );
--- react-app/counter-app/src/components/navbar.jsx
+++ react-app/counter-app/src/components/navbar.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
+// stateless functional component
 
-class NavBar extends Component {
-	render() {
-		return (
-			<nav className="navbar navbar-light bg-light">
-				<a className="navbar-brand" href="#">
-					Navbar
-				</a>
-			</nav>
-		);
-	}
-}
+const NavBar = props => {
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<a className="navbar-brand" href="#">
+				<span className="badge badge-pill badge-secondary">
+					{props.totalCounters}
+				</span>
+			</a>
+		</nav>
+	);
+};
 
 export default NavBar;
