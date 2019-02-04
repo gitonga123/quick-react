0548326cf873eba8912e8a25afd7f60717281d5f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-04 15:28:39 +0300

    Node Callbacks and Error Handling :simple-smile:

--- hku/index.js
+++ hku/index.js
@@ -1,4 +1,20 @@
-var rect = {
-  perimeter: (x, y) => (2 * (x + 7)),
-  area: (x, y) => (x * y)
-};
+const rect = require('./rectangle')
+
+function solveRect(l, b) {
+  // console.log('Solving the reactangle with l = ' + l);
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log('ERROR: ', err.message)
+    } else {
+      console.log(`Area of Rectangle ${l} by ${b} = ${rectangle.area()}`)
+      console.log(`Perimeter of Rectangle ${l} by ${b} = ${rectangle.perimeter()}`)
+    }
+  })
+  console.log("This statement is after the rect");
+}
+
+solveRect(5, 7)
+// solveRect(5, 10)
+// solveRect(15, 67)
+// solveRect(-15, 7)
+// solveRect(15, 0)
