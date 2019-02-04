8f5a589dbec0152c5b591b053ac71ca653926496
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-04 13:28:34 +0300

    JS Modules :simple-smiley:

--- hku/index.js
+++ hku/index.js
@@ -1,15 +1,4 @@
-const rect = require('./rectangle');
-
-function solveRect(l, b) {
-  // console.log('Solving the reactangle with l = ' + l);
-  if (l <= 0 || b <= 0) {
-    console.log('Reactangle dimensions should be greater than 0');
-  } else {
-    console.log(`The area of ${l} and ${b} the rectangle is ` + rect.area(l, b));
-    console.log(`The Perimeter of ${l} and ${b} the reactangle is ` + rect.perimeter(l, b));
-  }
-}
-
-solveRect(5, 7);
-solveRect(5, 10);
-solveRect(15, 67)
+var rect = {
+  perimeter: (x, y) => (2 * (x + 7)),
+  area: (x, y) => (x * y)
+};
