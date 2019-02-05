3f4dc49d85091a71f05c806deee1c788cc3c4c91
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-05 12:07:07 +0300

    Node HTTP Requests - GET :simple-smiley:

--- hku/index.js
+++ hku/index.js
@@ -1,14 +1,48 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
 const hostname = 'localhost'
 const port = 3000
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers)
-  console.log(req.body)
+  console.log(`Request for ${req.url} by method`)
+  if (req.method == 'GET') {
+    var fileUrl
+    if (req.url == '/') {
+      fileUrl = '/index.html'
+    } else {
+      fileUrl = req.url
+    }
 
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end(`<html><body>Hello World</body></html>`)
+    var filePath = path.resolve('./' + fileUrl)
+
+    const fileExt = path.extname(filePath);
+    if (fileExt == '.html') {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html')
+          res.end('<html><body><h1>Error 404 </h1></body></html>')
+        } else {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'text/html')
+          fs.createReadStream(filePath).pipe(res);
+          return;
+        }
+      })
+    } else {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/html')
+      res.end('<html><body><h1>Error 404</h1><h3>File Not an Html File</h3></body></html>')
+      return;
+    }
+  } else {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><body><h1>Error 503 - action not supported</h1></body></html>')
+    return;
+  }
 })
 
 server.listen(port, hostname, () => {
