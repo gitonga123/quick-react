89cde0798c9abe56ed9a72b0e835faf74cc58525
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-07 13:34:27 +0300

    Handle REST request - Express Router :ambulance:

--- hku/index.js
+++ hku/index.js
@@ -3,7 +3,8 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const dishRouter = require('./routes/dishRouter');
-
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = "localhost";
 const port = 3000;
@@ -13,35 +14,10 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter)
+app.use('/leaders', leaderRouter)
+app.use('/promotions', promoRouter)
 
 app.use(express.static(__dirname + "/public"));
-
-// handle get requests
-app.get("/dishes/:dishId", (req, res, next) => {
-  res.end(`Will send details of the dishes ${req.params.dishId}`);
-});
-
-// handle post request
-app.post("/dishes/:dishId", (req, res, next) => {
-  res.statusCode = 403; // operation not supported
-  res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
-});
-
-// handle put request
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write(`Updating details of the dish: ${req.params.dishID}\n`);
-  res.end(
-    'Will Update the dish: ' +
-    req.body.name +
-    ' with details ' +
-    req.body.description
-  );
-});
-
-// handle put request
-app.delete('/dishes/:dishId', (req, res, next) => {
-  res.end('Deleting dish: ' + req.params.dishId);
-});
 
 app.use((req, res, next) => {
   res.statusCode = 200;
--- hku/routes/dishRouter.js
+++ hku/routes/dishRouter.js
@@ -26,4 +26,25 @@ dishRouter.route('/')
     res.end(`Deleting all the dishes`);
   })
 
+dishRouter.route('/:dishId').get((req, res, next) => {
+  // handle get requests
+  res.end(`Will send details of the dish ${req.params.dishId}`);
+}).post((req, res, next) => {
+  // handle post request
+  res.statusCode = 403; // operation not supported
+  res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
+}).put((req, res, next) => {
+  // handle put request
+  res.write(`Updating details of the dish: ${req.params.dishID}\n`);
+  res.end(
+    'Will Update the dish: ' +
+    req.body.name +
+    ' with details ' +
+    req.body.description
+  );
+}).delete((req, res, next) => {
+  // handle Delete request
+  res.end('Deleting the dish: ' + req.params.dishId);
+});
+
 module.exports = dishRouter
