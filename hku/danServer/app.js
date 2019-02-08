6fd74e19e7286a915d5f5b0f813d8bb003cb9384
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-08 15:30:17 +0300

    Node MongoDB Connection :simple-smiley:

--- hku/danServer/app.js
+++ hku/danServer/app.js
@@ -3,6 +3,44 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbname = 'conFusion';
+
+MongoClient.connect(url, (error, client) => {
+  assert.equal(error, null);
+  console.log('Connected Correctly to Server');
+
+  const db = client.db(dbname);
+
+  const collection = db.collection('dishes');
+
+  collection.insertOne({
+    'name': 'Uthappizza',
+    'description': 'testing insert'
+  }, (error, result) => {
+    assert.equal(error, null);
+
+    console.log('After Insert: \n');
+    console.log(result.ops);
+
+    collection.find({}).toArray((error, docs) => {
+      assert.equal(error, null)
+
+      console.log('Found: \n')
+      console.log(docs);
+
+      db.dropCollection('dishes', (error, result) => {
+        assert.equal(error, null)
+
+        client.close();
+      })
+    })
+  });
+
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
