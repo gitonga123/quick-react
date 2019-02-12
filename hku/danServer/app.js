02953ac3b5d0acbee07cb739f0c5fcef2f8dd6fc
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-12 14:45:33 +0300

    Using Promises to negate Callback Hell :fire:

--- hku/danServer/app.js
+++ hku/danServer/app.js
@@ -3,50 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-const url = 'mongodb://localhost:27017';
-const dbname = 'conFusion';
-
-MongoClient.connect(url, (error, client) => {
-  assert.equal(error, null);
-  console.log('Connected Correctly to Server');
-
-  const db = client.db(dbname);
-
-  const collection = db.collection('dishes');
-
-  collection.insertOne({
-    'name': 'Uthappizza',
-    'description': 'testing insert'
-  }, (error, result) => {
-    assert.equal(error, null);
-
-    console.log('After Insert: \n');
-    console.log(result.ops);
-
-    collection.find({}).toArray((error, docs) => {
-      assert.equal(error, null)
-
-      console.log('Found: \n')
-      console.log(docs);
-
-      db.dropCollection('dishes', (error, result) => {
-        assert.equal(error, null)
-
-        client.close();
-      })
-    })
-  });
-
-});
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dboper = require('./operation.js');
+
+const url = 'mongodb://localhost:27017';
+const dbname = 'conFusion';
 
 var app = express();
 
@@ -83,5 +50,50 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+MongoClient.connect(url).then((client) => {
+
+    console.log('Connected correctly to server');
+    const db = client.db(dbname);
+
+    dboper.insertDocument(db, {
+          name: "Vadonut",
+          description: "Test"
+        },
+        "dishes")
+      .then((result) => {
+        console.log("Insert Document:\n", result.ops);
+
+        return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
+        console.log("Found Documents:\n", docs);
+
+        return dboper.updateDocument(db, {
+          name: "Vadonut"
+        }, {
+          description: "Updated Test"
+        }, "dishes");
+
+      })
+      .then((result) => {
+        console.log("Updated Document:\n", result.result);
+
+        return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
+        console.log("Found Updated Documents:\n", docs);
+
+        return db.dropCollection("dishes");
+      })
+      .then((result) => {
+        console.log("Dropped Collection: ", result);
+
+        return client.close();
+      })
+      .catch((err) => console.log(err));
+
+  })
+  .catch((err) => console.log(err));
 
 module.exports = app;
--- hku/danServer/operation.js
+++ hku/danServer/operation.js
@@ -2,44 +2,25 @@ const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
   const coll = db.collection(collection);
-
-  coll.insert(document, (error, result) => {
-    assert.equal(error, null);
-    console.log(`Inserted ${result.result.n} 
-                documents into the collection ${collection}`);
-    callback(result);
-  });
-
+  return coll.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
   const coll = db.collection(collection);
 
-  coll.find({}).toArray((error, docs) => {
-    assert.equal(error, null);
-    callback(docs);
-  })
+  return coll.find({}).toArray()
 
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
   const coll = db.collection(collection);
 
-  coll.deleteOne(document, (error, result) => {
-    assert.equal(error, null);
-
-    console.log(`Removed the document ${document}`);
-    callback(result);
-  })
+  return coll.deleteOne(document);
 };
 
 exports.updateDocuments = (db, document, update, collection, callback) => {
   const coll = db.collection(collection);
-  coll.updateOne(document, {
+  return coll.updateOne(document, {
     $set: update
-  }, null, (error, result) => {
-    assert.equal(error, null);
-    console.log(`Update the document with ${update}`);
-    callback(result);
-  });
+  }, null);
 };
