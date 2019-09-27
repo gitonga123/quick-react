f4a6d6e41362e2ca7173351454cfd08a19448fc8
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-09-27 17:42:24 +0300

    Quiz Complete

--- vue-skills/src/App.vue
+++ vue-skills/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :numCorrect="numCorrect" :numTotal="numTotal" />
     <b-container class="bv-example-row">
       <b-row>
-        <b-col sm="6" offset="n3">
-          <QuestionBox />
+        <b-col offset="n3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
         </b-col>
       </b-row>
     </b-container>
@@ -20,10 +25,43 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++;
+      }
+      this.numTotal++
+    }
+  },
+  mounted: function() {
+    fetch("http://127.0.0.1:88/api/v1/my-questions", {
+      method: "get"
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.questions = jsonData.data.questions;
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
-</style> **/
+</style> 
--- vue-skills/src/components/QuestionBox.vue
+++ vue-skills/src/components/QuestionBox.vue
@@ -1,14 +1,107 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-      <template v-slot:lead>Our Questions</template>
+      <template v-slot:lead>{{ currentQuestion.question }}</template>
 
       <hr class="my-4" />
-
-      <p>List of Answers</p>
-
-      <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-list-group>
+        <b-list-group-item
+          v-for="answer in answers"
+          :key="answer.id"
+          @click="selectedAnswer(answer.id)"
+          :class="[answerClass(answer.id)]"
+        >{{ answer.title }}</b-list-group-item>
+      </b-list-group>
+      <hr class="my-4" />
+      <b-button variant="primary" @click="submitAnswer" :disabled="selectedIndex === null || answered">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+import _ from "lodash";
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data() {
+    return {
+      selectedIndex: null,
+      shuffledAnswers: [],
+      correctIndex: 0,
+      answered: false,
+      
+    };
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.answers];
+      return answers;
+    }
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.answered = false;
+        this.shuffleAnswers();
+      }
+    }
+  },
+  methods: {
+    selectedAnswer(index) {
+      this.selectedIndex = index;
+    },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.answers];
+      this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.currentQuestion.answer.answer_id
+    },
+    submitAnswer() {
+      let isCorrect = false;
+      this.correctIndex = this.currentQuestion.answer.answer_id;
+      if (this.selectedIndex == this.correctIndex) {
+        isCorrect = true;
+      }
+      this.answered = true
+      this.increment(isCorrect);
+    },
+    answerClass(index) {
+      let answerClass =  '';
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = 'selected'
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = 'correct'
+      } else if (this.answered && this.selectedIndex == index && this.correctIndex !== index) {
+        answerClass = "incorrect"
+      }
+      return answerClass;
+    }
+  },
+  mounted() {
+    this.shuffleAnswers();
+  }
+};
+</script>
+<style  scoped>
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
+
+.selected {
+  border: 3px solid blue;
+  background-color: lightblue;
+}
+.correct {
+  border: 3px solid green;
+  background-color: lightgreen;
+}
+.incorrect {
+  border: 3px solid red;
+}
+</style>
